2656f7f08d6acaa8c48b2ca8bf0959caec416fd4
Daniel <daniellucas382@gmail.com>
2022-03-14 13:20:11 -0300

    create /home resource

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,33 @@
+import { config } from './config.js'
+import { logger } from './utils.js'
+import { Controller } from './controller.js'
+
+const controller = new Controller()
+const {
+  location,
+  pages: {
+    homeHTML
+  }
+} = config
+
+const routes = {
+  'GET:/': async function (request, response) {
+    response.writeHead(302, { Location: location.home })
+    response.end()
+  },
+
+  'GET:/home': async function (request, response) {
+    const { stream } = await controller.getFileStream(homeHTML)
+
+    // response padrão 'text/html '
+    return stream.pipe(response)
+  }
+}
+
 export function handler (request, response) {
-  return response.end('Hello World!')
+  const { method, url } = request
+  const httpPath = `${method}:${url}`
+
+  return routes[httpPath](request, response)
+    .catch(error => logger.error(`Error: ${error.stack}`))
 }
