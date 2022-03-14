24b30c9e9bd14a876366e599a0c13cb336008c23
Daniel <daniellucas382@gmail.com>
2022-03-14 12:28:29 -0300

    logging with logger

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import { server } from './server.js'
+import { logger } from './utils.js'
 
 server.listen(3000)
-  .on('listening', () => console.log(`Server listening on http://localhost:3000`))
+  .on('listening', () => logger.info(`Server listening on http://localhost:3000`))
