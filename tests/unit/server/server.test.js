621cb8719506df9a7004c7232684967f7c924c5c
Daniel <daniellucas382@gmail.com>
2022-03-15 08:09:27 -0300

    test GET /assets with index.html

--- tests/unit/server/server.test.js
+++ tests/unit/server/server.test.js
@@ -5,7 +5,7 @@ import { Controller } from '../../../server/controller.js'
 import { handler } from '../../../server/routes.js'
 import { TestUtils } from '../_utils/utils.js'
 
-const { pages, location } = config
+const { pages, location, constants: { CONTENT_TYPE } } = config
 
 describe('#Routes', function () {
   describe('test suit for api response', function () {
@@ -67,7 +67,32 @@ describe('#Routes', function () {
       expect(mockFileStream.pipe).toBeCalledWith(params.response)
     })
 
-    test.todo('GET /file.ext - should respond with a file stream')
+    test('GET /index.html - should respond with a file stream', async function () {
+      const expectedFilePath = '/index.html'
+      const expectedType = '.html'
+      const expectedHeader = { 'Content-Type': CONTENT_TYPE[expectedType]}
+
+      const mockFileStream = TestUtils.generateReadableStream(['any data'])
+      const params = TestUtils.defaultHandleParams()
+
+      params.request.method = 'GET'
+      params.request.url = expectedFilePath
+
+
+      jest
+        .spyOn(Controller.prototype, 'getFileStream')
+        .mockResolvedValue({ stream: mockFileStream, type: expectedType })
+
+      jest
+        .spyOn(mockFileStream, 'pipe')
+        .mockReturnValue()
+
+      await handler(...params.values())
+
+      expect(Controller.prototype.getFileStream).toBeCalledWith(expectedFilePath)
+      expect(mockFileStream.pipe).toBeCalledWith(params.response)
+      expect(params.response.writeHead).toBeCalledWith(200, expectedHeader)
+    })
 
     test.todo('GET /unknown - given an inexistent route it should respond with 404')
 
