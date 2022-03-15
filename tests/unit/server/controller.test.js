ce56569d1b2c82e65efdbe1b6511f30faa58c385
Daniel <daniellucas382@gmail.com>
2022-03-15 16:49:07 -0300

    planning tests

---
+++ tests/unit/server/controller.test.js
@@ -0,0 +1,9 @@
+import { jest, expect, describe, test, beforeEach } from '@jest/globals'
+
+describe('#Controller - test suit ', function () {
+  describe('getFileStream', function () {
+    test.todo('shoud throw if Service.getFileStream throws')
+
+    test.todo('shoud return correct stream and type')
+  })
+})
