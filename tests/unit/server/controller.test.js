0bca1d7fd4b8ec40d169890eba22c951609aa97a
Daniel <daniellucas382@gmail.com>
2022-03-15 16:53:59 -0300

    ensure Controller.getFileStream throws if Service.getFileStream throws

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -1,8 +1,23 @@
-import { jest, expect, describe, test, beforeEach } from '@jest/globals'
+import { jest, expect, describe, test } from '@jest/globals'
+
+import { Controller } from '../../../server/controller.js'
+import { Service } from '../../../server/service.js'
 
 describe('#Controller - test suit ', function () {
   describe('getFileStream', function () {
-    test.todo('shoud throw if Service.getFileStream throws')
+    test('shoud throw if Service.getFileStream throws', function () {
+      const controller = new Controller()
+
+      jest
+        .spyOn(Service.prototype, Service.prototype.getFileStream.name)
+        .mockImplementation(function () {
+          throw new Error('ANY_ERROR')
+        })
+
+      const fileStream = controller.getFileStream('any_file')
+
+      expect(fileStream).rejects.toThrow(new Error('ANY_ERROR'))
+    })
 
     test.todo('shoud return correct stream and type')
   })
