9336db4751856c3015a3602be6ddf0a0ec13fcdc
Daniel <daniellucas382@gmail.com>
2022-03-15 17:00:55 -0300

    ensure Controller.getFileStream throws if no file name is provided

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -1,10 +1,15 @@
-import { jest, expect, describe, test } from '@jest/globals'
+import { jest, expect, describe, test, beforeEach } from '@jest/globals'
 
 import { Controller } from '../../../server/controller.js'
 import { Service } from '../../../server/service.js'
 
 describe('#Controller - test suit ', function () {
   describe('getFileStream', function () {
+    beforeEach(function () {
+      jest.resetAllMocks()
+      jest.clearAllMocks()
+    })
+
     test('shoud throw if Service.getFileStream throws', function () {
       const controller = new Controller()
 
@@ -17,6 +22,21 @@ describe('#Controller - test suit ', function () {
       const fileStream = controller.getFileStream('any_file')
 
       expect(fileStream).rejects.toThrow(new Error('ANY_ERROR'))
+    })
+
+    test('shoud throw if no file name is provided', function () {
+      const controller = new Controller()
+      const service = new Service()
+
+      jest
+        .spyOn(service, 'getFileStream')
+        .mockImplementation(function () {
+          throw new Error('NO_FILE_NAME')
+        })
+
+      const fileStream = controller.getFileStream()
+
+      expect(fileStream).rejects.toThrow(new Error('NO_FILE_NAME'))
     })
 
     test.todo('shoud return correct stream and type')
